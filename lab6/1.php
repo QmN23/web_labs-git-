08246748793e1b896aac3e886914c325cf1559a3
QmN23 <jokerbeagle@mail.ru>
2025-05-13 17:16:29 +0200

    добавлен POST запрос
    * Добавил CURLOPT_SSL_VERIFYPEER для корректной работы с сертификатом
    * Добавил POST-запрос

--- lab6/1.php
+++ lab6/1.php
@@ -2,16 +2,46 @@
 
 // GET-запрос
 // Инициация cURL-сеанса
+$url = "https://jsonplaceholder.typicode.com/posts/1";
+$ch = curl_init($url);
+
+// Устанавливаем URL-адрес
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Передаем страницу браузеру и получаем ответ
+$ex = curl_exec($ch);
+// Закрываем сессию
+curl_close($ch);
+
+// Выводим результат GET-запроса
+echo "GET Response:\n$ex\n";
+
+// POST-запрос
+// Инициация cURL-сеанса
 $url = "https://jsonplaceholder.typicode.com/posts";
 $ch = curl_init($url);
 
+$data = [
+    'title' => 'Text',
+    'body' => 'text',
+    'userId' => 1
+];
 
-// Устанавливаем URL-адрес
-curl_setopt($ch, CURLOPT_URL, $url);
+// Настройка параметров для POST-запроса
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
-// Передаем страницу браузеру
+// Выполнение POST-запроса и получение ответа
 $ex = curl_exec($ch);
 
-//Закрываем
+// Закрытие сессии cURL
 curl_close($ch);
-echo "GET Response:\n$ex\n\n";
+
+// Выводим результат POST-запроса
+echo "POST Response:\n$ex\n";
+
