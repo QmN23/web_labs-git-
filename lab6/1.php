3f8ad9f6b2e851ebc7a94b9f3b76b71dc3345a03
QmN23 <jokerbeagle@mail.ru>
2025-05-13 17:33:25 +0200

    PUT&DELETE запросы
    * добавил обработку ошибок

--- lab6/1.php
+++ lab6/1.php
@@ -45,3 +45,46 @@ curl_close($ch);
 // Выводим результат POST-запроса
 echo "POST Response:\n$ex\n";
 
+// PUT-запрос
+
+// Обновленная data
+$newData = [
+    'id' => 1,
+    'title' => 'Text',
+    'body' => 'TTText',
+    'userId' => 1
+];
+
+$url = "https://jsonplaceholder.typicode.com/posts/1";
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($newData));
+
+$ex = curl_exec($ch);
+
+curl_close($ch);
+echo "PUT Response:\n$ex\n";
+
+
+// === DELETE-запрос ===
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$ex = curl_exec($ch);
+curl_close($ch);
+
+echo "DELETE Response:\n$ex\n";
+
+$info = curl_getinfo($ch);
+echo "HTTP статус: " . $info['http_code'] . "\n";
+
+if(curl_errno($ch)) {
+    echo 'cURL error: ' . curl_error($ch);
+}
